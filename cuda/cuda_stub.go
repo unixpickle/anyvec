d36814099fb0b1359105aac21d2abc22f44a702a
Alex Nichol <unixpickle@gmail.com>
2017-01-14 22:14:07 -0500

    added missing nocuda stub

--- cuda/cuda_stub.go
+++ cuda/cuda_stub.go
@@ -43,6 +43,10 @@ func (h *Handle) sgemm(transA, transB bool, m, n, k int, alpha float32, a unsafe
 	panic("unimplemented")
 }
 
+func (h *Handle) mul(n int, a, b unsafe.Pointer) {
+	panic("unimplemented")
+}
+
 // A buffer is an on-device memory buffer.
 type buffer struct {
 	size int
