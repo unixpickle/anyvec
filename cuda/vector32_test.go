afc0b9588fdd99edaedc334badd49b1faee4a3de
Alex Nichol <unixpickle@gmail.com>
2017-01-14 21:52:11 -0500

    benchmark unoptimized component-wise funcs

--- cuda/vector32_test.go
+++ cuda/vector32_test.go
@@ -73,6 +73,62 @@ func BenchmarkGemmMat(b *testing.B) {
 	}
 }
 
+func BenchmarkMul(b *testing.B) {
+	h, err := NewHandle()
+	if err != nil {
+		b.Fatal(err)
+	}
+	c := NewCreator32(h)
+	v1 := randomVector(c, 1024)
+	scale := make([]float32, v1.Len())
+	for i := range scale {
+		scale[i] = rand.Float32()*0.005 + 0.9975
+	}
+	v2 := c.MakeVectorData(scale)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		v1.Mul(v2)
+	}
+}
+
+func BenchmarkDiv(b *testing.B) {
+	h, err := NewHandle()
+	if err != nil {
+		b.Fatal(err)
+	}
+	c := NewCreator32(h)
+	v1 := randomVector(c, 1024)
+	scale := make([]float32, v1.Len())
+	for i := range scale {
+		scale[i] = rand.Float32()*0.005 + 0.9975
+	}
+	v2 := c.MakeVectorData(scale)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		v1.Div(v2)
+	}
+}
+
+func BenchmarkExp(b *testing.B) {
+	h, err := NewHandle()
+	if err != nil {
+		b.Fatal(err)
+	}
+	c := NewCreator32(h)
+	vec := make([]float32, 1024)
+	for i := range vec {
+		// Nice choice of values since -e^vec[i] = vec[i]
+		vec[i] = -0.56714329041
+	}
+	v := c.MakeVectorData(vec)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// Prevent numbers from exploding or changing too much.
+		v.Exp()
+		v.Scale(-1)
+	}
+}
+
 func randomVector(c anyvec32.Creator, size int) anyvec32.Vector {
 	d := make([]float32, size)
 	for i := range d {
